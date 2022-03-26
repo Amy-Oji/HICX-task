fca06fca5292810926fb658d5a3760138fae01a5
Amy-Oji <amyojiakor@gmail.com>
2022-03-26 16:41:47 +0100

    added the updated UML diagram

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,23 +7,33 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+//        checks if a directory path was passed in
+//        'working directory' field of the Run/Debug configuration environment
+
+
         if (args.length == 0){
-            System.out.println(" Please provide a directory in the program arguements ");
+            System.out.println(" Please provide a directory in the program arguments ");
             return;
         }
 
-        Path testPath = Path.of(args[0]);
-        if (!Files.exists(testPath)){
+        Path passedDirectoryPath = Path.of(args[0]);
+//        checks if the passed path is a valid directory path
+        if (!Files.exists(passedDirectoryPath)){
             System.out.println("Directory does not exist ");
             return;
         }
 
-        boolean exists = Files.exists(testPath);
+        boolean exists = Files.exists(passedDirectoryPath);
+
+//        prints true if the passed directory path is valid
+
         System.out.println("folder exists = " + exists);
 
-        MonitorDirectoryService monitor = new MonitorDirectory(testPath);
-        monitor.monitorDirectory();
+        MonitorDirectoryService monitor = new MonitorDirectory(passedDirectoryPath);
 
+//        calls the method that runs the application
+        monitor.monitorDirectory();
 
     }
 }
--- src/main/java/directoryMonitor/MonitorDirectory.java
+++ src/main/java/directoryMonitor/MonitorDirectory.java
@@ -31,11 +31,14 @@ public class MonitorDirectory implements MonitorDirectoryService {
     @Override
     public void monitorDirectory() {
 
-//        checking for processed folder
+//      first creates the processed folder if it is absent
         createProcessedFolder(passedDirectory);
 
         try {
+//            prints stats for existing files
             printStatistics(passedDirectory);
+
+//            the watchService is assigned to a method that monitors the directory
             watchService = FileSystems.getDefault().newWatchService();
             passedDirectory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
 
@@ -45,6 +48,7 @@ public class MonitorDirectory implements MonitorDirectoryService {
                     System.out.println("New directory event: filet-name::"+file+ " just added");
                     System.out.println("Statistics=========================================");
                     createProcessedFolder(passedDirectory);
+//                    loops through create-events and prints the stats for new files
                     printStatistics(passedDirectory);
 
                 }
@@ -52,19 +56,22 @@ public class MonitorDirectory implements MonitorDirectoryService {
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
         }
     }
 
     @Override
     public void printStatistics(Path passedDirectory) throws IOException {
+//
         try{
-          var files = Files.newDirectoryStream(passedDirectory, "*.txt");
+//            stream of filePaths in the passedDirectory
+            DirectoryStream<Path> files = Files.newDirectoryStream(passedDirectory, "*.txt");
             List<StatsProcessorService> statsProcessorServices = new ArrayList<>();
             statsProcessorServices.add(new TotalWordsStats());
             statsProcessorServices.add(new MostUsedWordStat());
             statsProcessorServices.add(new DotsCountStats());
+
+//            looping through the filePaths and calls the processStats method on each file
+
           files.forEach(filePath -> {
               fileContent = txtFileProcessor.readFile(filePath);
               Path fileName = filePath.getFileName();
@@ -80,6 +87,7 @@ public class MonitorDirectory implements MonitorDirectoryService {
 
     @Override
     public void createProcessedFolder(Path pathToPassedDirectory) {
+//        checks if processedFolder has been created and creates the folder if it has not been created
         processedFolder = pathToPassedDirectory.resolve("processedFolder");
         try {
             if (!Files.exists(processedFolder)) {
@@ -92,6 +100,7 @@ public class MonitorDirectory implements MonitorDirectoryService {
 
     @Override
     public void moveProcessedFiles(Path filePath) {
+//        method to move file
         try {
             Files.move(filePath, processedFolder.resolve(filePath.getFileName().toString()), StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
